14a9e7823811d156e9e57e35d51fa3bded530410
Martin G. Schulz <dev@mgschulz.de>
2025-08-09 10:47:30 +0200

    add node-picker

---
+++ mne_nodes/gui/node/node_picker.py
@@ -0,0 +1,17 @@
+"""
+Authors: Martin Schulz <dev@mgschulz.de>
+License: BSD 3-Clause
+Github: https://github.com/marsipu/mne-nodes
+"""
+
+from PySide6.QtWidgets import QDockWidget, QTabWidget
+
+
+class NodePicker(QDockWidget):
+    def __init__(self, controller, parent=None):
+        super().__init__(parent)
+        self.ct = controller
+        self.tab_widget = QTabWidget()
+        self.setWindowTitle("Node Picker")
+        self.setGeometry(100, 100, 300, 200)
+        self.setWidget(self.tab_widget)
