d80fde874bdf56c149b073ebaf8bd95034063c11
Martin G. Schulz <dev@mgschulz.de>
2026-04-30 07:11:07 +0200

    add faulthandler

--- mne_nodes/development/run_function_Importer.py
+++ mne_nodes/development/run_function_Importer.py
@@ -7,13 +7,16 @@ GitHub: https://github.com/marsipu/mne-nodes
 import sys
 from pathlib import Path
 
+import faulthandler
+
 from qtpy.QtWidgets import QApplication
 from mne_nodes.gui.function_widgets import FunctionImporter
 from mne_nodes.pipeline.streams import init_logging
+
+faulthandler.enable()
 
 init_logging()
 app = QApplication(sys.argv)
 file_path = Path(__file__).parent.parent / "core_functions" / "core_functions.py"
 fi = FunctionImporter(file_path=file_path, allow_exec=True)
-fi.show()
 sys.exit(app.exec())
